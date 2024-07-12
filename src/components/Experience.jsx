2dc99e232b448513ea7fee8184f7ee4cb9ae4376
Nadinda <nadinda@nadinda.dev>
2024-07-12 17:07:47 +0200

    try

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,22 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Html } from "@react-three/drei";
+import { TableSetup } from "./TableSetup";
+import NeonLogo from "../assets/neon-green.png";
 
 const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight intensity={1} />
+      <OrbitControls makeDefault />
+
+      {/* Table */}
+      <TableSetup />
+      {/* <PerspectiveCamera makeDefault fov={3} position={[60, 20, 10]} /> */}
+      <PerspectiveCamera makeDefault fov={2.5} position={[40, 20, -40]} />
+
+      {/* Neon Light */}
+      <Html makeDefault>
+        <img src={NeonLogo} alt="Neon Light Green" />
+      </Html>
     </>
   );
 };
