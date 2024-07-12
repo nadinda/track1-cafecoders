511f510cbf43175be2b831518d491e71fdc32045
Nadinda <nadinda@nadinda.dev>
2024-07-12 12:33:08 +0200

    feat: add Navbar and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Events from "./components/Events";
+import Menu from "./components/Menu";
+import Collections from "./components/Collections";
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import Experience from "./components/Experience";
 
 function App() {
   return (
-    <Canvas>
-      <Experience />
-    </Canvas>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/collections" element={<Collections />} />
+      </Routes>
+    </Router>
   );
 }
 
